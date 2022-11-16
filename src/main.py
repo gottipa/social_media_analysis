10dbcae1c7959c6b1784caffb8bfc73a3be605fd
gottipa <93541080+gottipa@users.noreply.github.com>
2022-11-16 15:11:54 +0530

    Add files via upload

---
+++ src/main.py
@@ -0,0 +1,72 @@
+"""Main module"""
+
+import ray
+import hydra
+from dateutil.parser import parse
+from omegaconf import DictConfig, OmegaConf
+from services.producer.app import App as Producer
+from services.consumer.app import App as Consumer
+from core.logger import logger as log
+from utils.helpers import init_start_end_date, build_search_entities
+
+
+@hydra.main(version_base=None, config_path="../conf", config_name="main")
+def main(conf: DictConfig) -> None:
+    log.debug(OmegaConf.to_yaml(conf))
+
+    run_mode = conf.run_mode.lower()
+    producer_conf = OmegaConf.to_object(conf.producer)
+    kafka_conf = OmegaConf.to_object(conf.kafka)
+    start_date, end_date = init_start_end_date()
+
+    if not ray.is_initialized():
+        ray.init()
+
+    actors = []
+    if run_mode == "producer":
+        if "start_date" in conf.producer.keys() and conf.producer.start_date:
+            start_date = parse(conf.producer.start_date).date()
+            producer_conf.pop("start_date", None)
+        if "end_date" in conf.producer.keys() and conf.producer.end_date:
+            end_date = parse(conf.producer.end_date).date()
+            producer_conf.pop("end_date", None)
+
+        log.info(f"Running with start_date: {start_date}, end_date: {end_date}")
+        search_entities = build_search_entities(conf.entity, start_date, end_date)
+
+        for entity in search_entities:
+            crawler_conf = producer_conf.copy()
+            crawler_conf.update({"entity": entity})
+            actors.append(
+                Producer.options(memory=150 * 1024 * 1024).remote(
+                    crawler_conf=crawler_conf,
+                    queue_conf=dict(config=kafka_conf),
+                )
+            )
+    elif run_mode == "consumer":
+        for topic in conf.consumer.topics:
+            bq_conf = OmegaConf.to_object(conf.bigquery).copy()
+            bq_conf.update({"table": topic, "model": topic})
+            # NOTE: to create more parallel consumers
+            for _ in range(conf.consumer.n_workers_per_topic):
+                actors.append(
+                    Consumer.options(memory=150 * 1024 * 1024).remote(
+                        topic=[topic],
+                        bq_conf=bq_conf,
+                        queue_conf=dict(config=kafka_conf),
+                    )
+                )
+    else:
+        raise ValueError(f"Unsupported run_mode: {run_mode}")
+
+    log.info(f"Creating {len(actors)} actors")
+    result_ids = [actor.run.remote() for actor in actors]
+    while len(result_ids):
+        done_ids, result_ids = ray.wait(result_ids)
+
+    # results = ray.get(result_ids)
+    ray.shutdown()
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/services/__init__.py
@@ -0,0 +1,4 @@
+"""Relative import services"""
+
+from .consumer.app import App as Consumer
+from .producer.app import App as Producer
